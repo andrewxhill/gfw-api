a74dc7c524aa5e731a8d181e077d98082a648876
Aaron <eightysteele@gmail.com>
2014-02-16 19:57:04 -0800

    tos link in pub emails, fix pub notify

--- gfw/pubsub.py
+++ gfw/pubsub.py
@@ -131,7 +131,7 @@ You can unsubscribe or manage your subscriptions by emailing: gfw@wri.org
 
 You will receive a separate e-mail for each distinct polygon, country, or shape on the GFW map. You will also receive a separate e-mail for each dataset for which you have requested alerts (FORMA alerts, Imazon SAD Alerts, and NASA QUICC alerts.)
 
-Please note that this information is subject to the Global Forest Watch <a href='http://globalforestwatch.com/about'>Terms of Service</a>.
+Please note that this information is subject to the Global Forest Watch <a href='http://globalforestwatch.com/terms'>Terms of Service</a>.
 """
 
         html = """You have subscribed to forest change alerts through Global Forest Watch. This message reports new forest change alerts for one of your areas of interest (a country or self-drawn polygon).
@@ -146,7 +146,7 @@ You can unsubscribe or manage your subscriptions by emailing: gfw@wri.org
 <p>
 You will receive a separate e-mail for each distinct polygon, country, or shape on the GFW map. You will also receive a separate e-mail for each dataset for which you have requested alerts (FORMA alerts, Imazon SAD Alerts, and NASA QUICC alerts.)
 <p>
-Please note that this information is subject to the Global Forest Watch <a href='http://globalforestwatch.com/about'>Terms of Service</a>.
+Please note that this information is subject to the Global Forest Watch <a href='http://globalforestwatch.com/terms'>Terms of Service</a>.
 """
 
         alert['interval'] = 'month'
@@ -174,7 +174,7 @@ Please note that this information is subject to the Global Forest Watch <a href=
                                 q=%s)' % (response.status_code,
                                           response.content,
                                           sql))
-            return body.format(**alert), html.format(**alert)
+        return body.format(**alert), html.format(**alert)
 
     def post(self):
         """"""
