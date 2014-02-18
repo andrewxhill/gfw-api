1cd20ed58f5529a7b2b42d2b7543537b9ce15665
Aaron <eightysteele@gmail.com>
2014-02-18 07:03:17 -0500

    order iso by size

--- tools/country_cache.py
+++ tools/country_cache.py
@@ -29,8 +29,16 @@ ANALYSIS_FILENAME = '%s_%s_%s_%s.json'
 
 
 def get_iso_codes():
-    """Return list of distinct ISO codes."""
-    sql = 'SELECT distinct(iso) FROM forma_api ORDER BY iso desc'
+    """Return list of distinct FORMA ISO codes ordered by country size."""
+    # Thanks @d4weed!
+    sql = """SELECT DISTINCT(forma_api.iso) as iso,
+                             world_countries.area as area
+             FROM world_countries
+             JOIN forma_api
+             ON
+             world_countries.iso3 = forma_api.iso
+             GROUP BY iso, area
+             ORDER BY area DESC"""
     params = urllib.urlencode(dict(q=sql))
     url = '%s%s' % (CDB_BASE_URL, params)
     return map(lambda x: x['iso'], requests.get(url).json()['rows'])
@@ -44,13 +52,11 @@ def date_range(begin, end, delta):
         d += delta
 
 
-def forma_dates():
+def forma_dates(begin=date(2006, 1, 1), end=date(2014, 2, 1),
+    delta=relativedelta(months=+1)):
     """Return sequence of date tuples for FORMA."""
-    begin = date(2006, 1, 1)
-    end = date(2014, 2, 1)
-    delta = relativedelta(months=+1)
     dates = itertools.product(date_range(begin, end, delta), repeat=2)
-    return filter(lambda x: x[0] <= x[1] and x[0] != x[1], dates)
+    return set(filter(lambda x: x[0] <= x[1] and x[0] != x[1], dates))
 
 
 def forma_cache_params():
@@ -61,51 +67,6 @@ def forma_cache_params():
                 yield fmt, iso, begin, end
 
 
-def cache_forma():
-    os.chdir('gcs')
-    for params in forma_cache_params():
-        fmt, iso, begin, end = params
-        url = API_ANALYSIS_URL % ('forma', iso, begin, end)
-        response = requests.get(url)
-        if not response.json()['value']:
-            print 'No alerts for %s' % url
-        else:
-            filename = ANALYSIS_FILENAME % ('forma', begin, end, iso)
-            with open(filename, 'wb') as fd:
-                for chunk in response.iter_content(chunk_size=10000):
-                    fd.write(chunk)
-            url = API_DOWNLOAD_URL % ('forma', fmt, iso, begin, end)
-            print url
-            filename = FILE_NAME % ('forma', begin, end, iso, fmt)
-            print filename
-            response = requests.get(url)
-            with open(filename, 'wb') as fd:
-                for chunk in response.iter_content(chunk_size=10000):
-                    fd.write(chunk)
-
-
-def test_forma():
-    max_cache = 2
-    count = 0
-    params = forma_cache_params()
-    while count < max_cache:
-        fmt, iso, begin, end = params.next()
-        url = API_ANALYSIS_URL % ('forma', iso, begin, end)
-        analysis = requests.get(url).json()
-        if not analysis['value']:
-            print 'No alerts for %s' % url
-            continue
-        url = API_DOWNLOAD_URL % ('forma', fmt, iso, begin, end)
-        print url
-        filename = FILE_NAME % ('forma', begin, end, iso, fmt)
-        print filename
-        count += 1
-        response = requests.get(url)
-        with open(filename, 'wb') as fd:
-            for chunk in response.iter_content(chunk_size=10000):
-                fd.write(chunk)
-
-
 #define a worker function
 def worker(queue):
     queue_full = True
@@ -113,6 +74,7 @@ def worker(queue):
         try:
             params = queue.get(False)
             fmt, iso, begin, end = params
+            print 'Processing %s...' % iso
             url = API_ANALYSIS_URL % ('forma', iso, begin, end)
             response = requests.get(url)
             if response.status_code != 200:
@@ -135,7 +97,7 @@ def worker(queue):
                 with open(filename, 'wb') as fd:
                     for chunk in response.iter_content(chunk_size=10000):
                         fd.write(chunk)
-                print filename
+                print 'Done: %s' % filename
         except Queue.Empty:
             queue_full = False
         except Exception, e:
@@ -150,7 +112,7 @@ if __name__ == '__main__':
     for params in forma_cache_params():
         q.put(params)
 
-    thread_count = 25
+    thread_count = 3
     for i in range(thread_count):
         t = threading.Thread(target=worker, args = (q,))
         t.start()
