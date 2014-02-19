ed07d2f2929a372da09de03549802c66f50e2181
Aaron <eightysteele@gmail.com>
2014-02-19 16:19:58 -0500

    hotfix: v2 cdb

--- gfw/cdb.py
+++ gfw/cdb.py
@@ -26,7 +26,7 @@ from google.appengine.api import urlfetch
 if runtime_config.get('cdb_endpoint'):
     ENDPOINT = runtime_config.get('cdb_endpoint')
 else:
-    ENDPOINT = 'http://wri-01.cartodb.com/api/v1/sql'
+    ENDPOINT = 'http://wri-01.cartodb.com/api/v2/sql'
 
 
 def _get_api_key():
